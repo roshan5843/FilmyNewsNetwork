8a09a750126c7de5c9697dc7f111fc7c9fd384fa
Roshan Yadav <yroshan584@gmail.com>
2024-05-17 11:06:31 +0545

    show the comment of the post

---
+++ client/src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import moment from 'moment'
+
+const Comment = ({ comment }) => {
+  const [user, setUser] = useState({})
+  useEffect(() => {
+    const getuser = async () => {
+      try {
+        const res = await fetch(`/api/user/${comment.userId}`)
+        const data = await res.json()
+        if (res.ok) {
+          setUser(data)
+        }
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    getuser()
+  }, [comment])
+  return (
+    <div className='flex p-4 border-b dark:border-green-600 text-sm'>
+      <div className='flex-shrink-0 mr-3'>
+        <img
+          className='w-10 h-10 rounded-full bg-gray-200'
+          src={user.profilePicture}
+          alt={user.username}
+        />
+      </div>
+      <div className='flex-1'>
+        <div className='flex items-center mb-1'>
+          <span className='font-bold mr-1 text-xs truncate'>{user ? `${user.username}` : 'anonymous user'}</span>
+          <span>{moment(comment.createdAt).fromNow()}</span>
+        </div>
+        <p className='text-gray-500 pb-2'>{comment.content}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Comment
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,13 +1,15 @@
 import { Alert, Button, Textarea } from 'flowbite-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Comment from './Comment'
 
 const CommentSection = ({ postId }) => {
   const { currentUser } = useSelector((state) => state.user)
   const [comment, setComment] = useState('')
   const [commentError, setCommentError] = useState(null)
-  const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState([])
+
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -25,17 +27,33 @@ const CommentSection = ({ postId }) => {
           postId,
           userId: currentUser._id,
         }),
-      });
-      const data = await res.json();
+      })
+      const data = await res.json()
       if (res.ok) {
-        setComment('');
-        setCommentError(null);
-        setComments([data, ...comments]);
+        setComment('')
+        setCommentError(null)
+        setComments([data, ...comments])
       }
     } catch (error) {
-      setCommentError(error.message);
+      setCommentError(error.message)
     }
-  };
+  }
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`)
+        if (res.ok) {
+          const data = await res.json()
+          setComments(data)
+        }
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    getComments()
+  }, [postId])
+
   return (
     <div>
       {currentUser ? (
@@ -87,6 +105,21 @@ const CommentSection = ({ postId }) => {
             </Alert>
           )}
         </form>
+      )}
+      {comments.length === 0 ? (
+        <p className='text-sm my-5'> No comments yet!! </p>
+      ) : (
+        <>
+          <div className='text-sm my-5 flex items-center gap-1'>
+            <p>Comments</p>
+            <div className='border border-gray-400 py-1 px-2 rounded-sm'>
+              <p>{comments.length}</p>
+            </div>
+            </div>
+            {comments.map(comment => (
+              <Comment key={comment.id} comment = {comment} />
+            ))}
+        </>
       )}
     </div>
   )
