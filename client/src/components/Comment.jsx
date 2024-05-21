6348b6b1cf32f65cd3cfeead01209128f838fdae
Roshan Yadav <yroshan584@gmail.com>
2024-05-21 13:12:29 +0545

    add delete fuctionality to comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -75,7 +75,7 @@ import { useSelector } from 'react-redux'
 import { Button, Textarea } from 'flowbite-react'
 import { set } from 'mongoose'
 
-const Comment = ({ comment, onLike, onEdit }) => {
+const Comment = ({ comment, onLike, onEdit, onDelete }) => {
   const [user, setUser] = useState({})
   const [isEditing, setIsEditing] = useState(false)
   const [editedContent, setEditedContent] = useState(comment.content)
@@ -134,64 +134,77 @@ const Comment = ({ comment, onLike, onEdit }) => {
         body: JSON.stringify({
           content: editedContent,
         }),
-      });
+      })
       if (res.ok) {
-        setIsEditing(false);
-        onEdit(comment, editedContent);
+        setIsEditing(false)
+        onEdit(comment, editedContent)
       }
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     }
-  };
-    return (
-      <div className='flex p-4 border-b dark:border-green-600 text-sm'>
-        <div className='flex-shrink-0 mr-3'>
-          <img
-            className='w-10 h-10 rounded-full bg-gray-200'
-            src={user.profilePicture}
-            alt={user.username}
-          />
+  }
+  return (
+    <div className='flex p-4 border-b dark:border-green-600 text-sm'>
+      <div className='flex-shrink-0 mr-3'>
+        <img
+          className='w-10 h-10 rounded-full bg-gray-200'
+          src={user.profilePicture}
+          alt={user.username}
+        />
+      </div>
+      <div className='flex-1'>
+        <div className='flex items-center mb-1'>
+          <span className='font-bold mr-1 text-xs truncate'>
+            {user ? `${user.username}` : 'anonymous user'}
+          </span>
+          <span>{moment(comment.createdAt).fromNow()}</span>
         </div>
-        <div className='flex-1'>
-          <div className='flex items-center mb-1'>
-            <span className='font-bold mr-1 text-xs truncate'>
-              {user ? `${user.username}` : 'anonymous user'}
-            </span>
-            <span>{moment(comment.createdAt).fromNow()}</span>
-          </div>
-          {isEditing ? (
-            <>
-              <Textarea
-                className='mb-2'
-                value={editedContent}
-                onChange={(e) => setEditedContent(e.target.value)}
-              />
-              <div className=" flex justify-end gap-2 text-xs">
-                <Button type='button' size='sm' gradientDuoTone='purpleToBlue' onClick={handleSave}>
-                  Save
-                </Button>
-                <Button type='button' size='sm' gradientDuoTone='purpleToBlue' outline onClick={() => setIsEditing(false)}>
-                  Cancle
-                </Button>
-              </div>
-            </>
-          ) : (
-            <>
-              <p className='text-gray-500 pb-2'>{comment.content}</p>
-              <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
-                <button
-                  type='button'
-                  onClick={handleLikeClick}
-                  className={`text-gray-400 hover:text-blue-500 ${userHasLiked && '!text-blue-500'
-                    }`}
-                >
-                  <FaThumbsUp className='text-sm' />
-                </button>
-                <p className='text-gray-400'>
-                  {likes > 0 && `${likes} ${likes === 1 ? 'like' : 'likes'}`}
-                </p>
-                {currentUser &&
-                  (currentUser._id === comment.userId || currentUser.isAdmin) && (
+        {isEditing ? (
+          <>
+            <Textarea
+              className='mb-2'
+              value={editedContent}
+              onChange={(e) => setEditedContent(e.target.value)}
+            />
+            <div className=' flex justify-end gap-2 text-xs'>
+              <Button
+                type='button'
+                size='sm'
+                gradientDuoTone='purpleToBlue'
+                onClick={handleSave}
+              >
+                Save
+              </Button>
+              <Button
+                type='button'
+                size='sm'
+                gradientDuoTone='purpleToBlue'
+                outline
+                onClick={() => setIsEditing(false)}
+              >
+                Cancle
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className='text-gray-500 pb-2'>{comment.content}</p>
+            <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+              <button
+                type='button'
+                onClick={handleLikeClick}
+                className={`text-gray-400 hover:text-blue-500 ${
+                  userHasLiked && '!text-blue-500'
+                }`}
+              >
+                <FaThumbsUp className='text-sm' />
+              </button>
+              <p className='text-gray-400'>
+                {likes > 0 && `${likes} ${likes === 1 ? 'like' : 'likes'}`}
+              </p>
+              {currentUser &&
+                (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                  <>
                     <button
                       type='button'
                       onClick={handleEdit}
@@ -199,14 +212,21 @@ const Comment = ({ comment, onLike, onEdit }) => {
                     >
                       Edit
                     </button>
-                  )}
-              </div>
-            </>
-          )}
-        </div>
+                    <button
+                      type='button'
+                      onClick={() => onDelete(comment._id)}
+                      className='text-gray-500 hover:text-red-500'
+                    >
+                      Delete
+                    </button>
+                  </>
+                )}
+            </div>
+          </>
+        )}
       </div>
-    )
-  }
-
+    </div>
+  )
+}
 
 export default Comment
