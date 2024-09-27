18b2c5ca8865460a395f8e89d8f4f36bf6aeb556
Roshan Yadav <yroshan584@gmail.com>
2024-09-27 12:40:59 +0545

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,31 +6,31 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import ThemeProvider from './components/ThemeProvider.jsx'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <PersistGate persistor={persistor}>
-//     <Provider store={store}>
-//       <ThemeProvider>
-//         <App />
-//       </ThemeProvider>
-//     </Provider>
-//   </PersistGate>
-// )
-
-const isSnap = navigator.userAgent === 'ReactSnap';
-
-ReactDOM.hydrateRoot(document.getElementById('root'), (
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
     <Provider store={store}>
-        {/* Disable PersistGate during pre-rendering */}
-        {isSnap ? (
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        ) : (
-            <PersistGate loading={null} persistor={persistor}>
-                <ThemeProvider>
-                    <App />
-                </ThemeProvider>
-            </PersistGate>
-        )}
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
-));
+  </PersistGate>
+)
+
+// const isSnap = navigator.userAgent === 'ReactSnap';
+// 
+// ReactDOM.hydrateRoot(document.getElementById('root'), (
+//     <Provider store={store}>
+//         {/* Disable PersistGate during pre-rendering */}
+//         {isSnap ? (
+//             <ThemeProvider>
+//                 <App />
+//             </ThemeProvider>
+//         ) : (
+//             <PersistGate loading={null} persistor={persistor}>
+//                 <ThemeProvider>
+//                     <App />
+//                 </ThemeProvider>
+//             </PersistGate>
+//         )}
+//     </Provider>
+// ));
